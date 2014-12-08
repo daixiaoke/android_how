4b6aed0f7e66e755d2612b0d2cebef37bb18613a
daixiaoke <xiaoke.dai@gmail.com>
2014-12-08 23:12:27 +0800

    add two simple android projects: Helloworld & ActivityTest

---
+++ Helloworld/src/com/example/helloworld/HelloworldActivity.java
@@ -0,0 +1,67 @@
+package com.example.helloworld;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.util.Log;
+
+
+
+public class HelloworldActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.helloworld_layout);
+        Log.d("HelloworldActivity", "onCreate execute");
+        Log.i("data", "hello, world");
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.helloworld, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_helloworld, container, false);
+            return rootView;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ android_how
 ===========
 
 my android how project
+
+1. 2014.12.08
+  add Helloworld & ActivityTest projects.
