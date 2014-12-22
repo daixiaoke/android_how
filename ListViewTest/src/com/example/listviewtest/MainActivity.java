dc7a506102dccf74a65eeda3bd6c40e91a8333fe
daixiaoke <xiaokedai@gmail.com>
2014-12-22 17:08:22 +0800

    complete ListViewTest

--- ListViewTest/src/com/example/listviewtest/MainActivity.java
+++ ListViewTest/src/com/example/listviewtest/MainActivity.java
@@ -1,30 +1,91 @@
 package com.example.listviewtest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-	private String[] data = {"Apple", "Banana", "Orange", "Watermelon",
+	/*private String[] data = {"Apple", "Banana", "Orange", "Watermelon",
 		"Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango",
 		"Mango1", "Mango2", "Mango3", "Mango4", "Mango5", "Mango6", "Mango7", 
 		"Mango8", "Mango9", "Mango10", "Mango11", "Mango12"};
+	*/
+	
+	private List<Fruit> fruitList = new ArrayList<Fruit>();
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-				MainActivity.this, android.R.layout.simple_list_item_1, data);
+		initFruits();
+		
+		FruitAdapter adapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item, fruitList);
+		
+		/*ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+				MainActivity.this, android.R.layout.simple_list_item_1, data);*/
 		ListView listView = (ListView) findViewById(R.id.list_view);
 		listView.setAdapter(adapter);
+		
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Fruit fruit = fruitList.get(position);
+				Toast.makeText(MainActivity.this, fruit.getName(), Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 
+	private void initFruits() {
+		Fruit apple = new Fruit("Apple", R.drawable.apple);
+		fruitList.add(apple);
+		Fruit banana = new Fruit("Banana", R.drawable.banana);
+		fruitList.add(banana);
+		Fruit orange = new Fruit("Orange", R.drawable.orange);
+		fruitList.add(orange);
+		Fruit watermelon = new Fruit("Watermelon", R.drawable.watermelon);
+		fruitList.add(watermelon);
+		Fruit pear = new Fruit("Pear", R.drawable.pear);
+		fruitList.add(pear);
+		Fruit grapes = new Fruit("Grapes", R.drawable.grapes);
+		fruitList.add(grapes);
+		Fruit pineapple = new Fruit("Pineapple", R.drawable.pineapple);
+		fruitList.add(pineapple);
+		Fruit strawberry = new Fruit("Strawberry", R.drawable.strawberry);
+		fruitList.add(strawberry);
+		Fruit cherry = new Fruit("Cherry", R.drawable.cherry);
+		fruitList.add(cherry);
+		Fruit mango = new Fruit("Mango0", R.drawable.mango);
+		fruitList.add(mango);
+		Fruit mango1 = new Fruit("Mango1", R.drawable.mango);
+		fruitList.add(mango1);
+		Fruit mango2 = new Fruit("Mango2", R.drawable.mango);
+		fruitList.add(mango2);
+		Fruit mango3 = new Fruit("Mango3", R.drawable.mango);
+		fruitList.add(mango3);
+		Fruit mango4 = new Fruit("Mango4", R.drawable.mango);
+		fruitList.add(mango4);
+		Fruit mango5 = new Fruit("Mango5", R.drawable.mango);
+		fruitList.add(mango5);
+		Fruit mango6 = new Fruit("Mango6", R.drawable.mango);
+		fruitList.add(mango6);
+		Fruit mango7 = new Fruit("Mango7", R.drawable.mango);
+		fruitList.add(mango7);
+		Fruit mango8 = new Fruit("Mango8", R.drawable.mango);
+		fruitList.add(mango8);
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- UISizeTest/gen/com/example/uisizetest/R.java
+++ UISizeTest/gen/com/example/uisizetest/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.example.listviewtest;
+package com.example.uisizetest;
 
 public final class R {
     public static final class attr {
@@ -22,29 +22,14 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int all234618=0x7f020000;
-        public static final int apple=0x7f020001;
-        public static final int banana=0x7f020002;
-        public static final int cherry=0x7f020003;
-        public static final int grapes=0x7f020004;
-        public static final int ic_launcher=0x7f020005;
-        public static final int mango=0x7f020006;
-        public static final int orange=0x7f020007;
-        public static final int pear=0x7f020008;
-        public static final int pineapple=0x7f020009;
-        public static final int sina_live_setup20130528=0x7f02000a;
-        public static final int strawberry=0x7f02000b;
-        public static final int watermelon=0x7f02000c;
+        public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int fruit_image=0x7f080001;
-        public static final int fruit_name=0x7f080002;
-        public static final int list_view=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int button=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fruit_item=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
