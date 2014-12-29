81fb3b37eaa59ddeab046814c7fbd2d7204cf915
daixiaoke <xiaokedai@gmail.com>
2014-12-29 16:49:42 +0800

    add project DatabaseTest

---
+++ DatabaseTest/src/com/example/databasetest/MyDatabaseHelper.java
@@ -0,0 +1,38 @@
+package com.example.databasetest;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+import android.content.Context;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+	public static final String CREATE_BOOK = "create table Book ("
+			+ "id integer primary key autoincrement, "
+			+ "author text, "
+			+ "price real, "
+			+ "pages integer, "
+			+ "name text)";
+	
+	private Context mContext;
+	
+	public MyDatabaseHelper(Context context, String name, CursorFactory factory, int version) {
+		super(context, name, factory, version);
+		mContext = context;
+	}
+	
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		db.execSQL(CREATE_BOOK);
+		Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
