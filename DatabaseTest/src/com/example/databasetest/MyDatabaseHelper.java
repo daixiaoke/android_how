434b10d8fc224ce57e64554f3693c3fcf4d79b6d
daixiaoke <xiaokedai@gmail.com>
2014-12-30 10:25:28 +0800

    add best update database

--- DatabaseTest/src/com/example/databasetest/MyDatabaseHelper.java
+++ DatabaseTest/src/com/example/databasetest/MyDatabaseHelper.java
@@ -13,12 +13,14 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 			+ "author text, "
 			+ "price real, "
 			+ "pages integer, "
-			+ "name text)";
+			+ "name text, "
+			+ "category_id integer)";
 	
 	public static final String CREATE_CATEGORY = "create table Category ("
 			+ "id integer primary key autoincrement, "
 			+ "category_name text, "
 			+ "category_code integer)";
+			
 	
 	private Context mContext;
 	
@@ -36,11 +38,19 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase db, int arg1, int arg2) {
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 		// TODO Auto-generated method stub
-		db.execSQL("drop table if exists Book");
-		db.execSQL("drop table if exists Categroy");
-		onCreate(db);
+		//db.execSQL("drop table if exists Book");
+		//db.execSQL("drop table if exists Category");
+		//onCreate(db);
+		switch(oldVersion) {
+		case 1:
+			db.execSQL(CREATE_CATEGORY);
+		case 2:
+			db.execSQL("alter table Book add column category_id integer");
+		default:
+			break;
+		}
 	}
 
 }
