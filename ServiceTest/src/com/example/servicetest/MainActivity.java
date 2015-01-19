84e79e2cc7ae8f95a057d14b2a7de48ab14ab8dc
daixiaoke <xiaokedai@gmail.com>
2015-01-19 22:14:45 +0800

    add communication between activity & service

--- ServiceTest/src/com/example/servicetest/MainActivity.java
+++ ServiceTest/src/com/example/servicetest/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.servicetest;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +17,25 @@ public class MainActivity extends Activity implements OnClickListener{
 	private Button startService;
 	private Button stopService;
 	
+	private Button bindService;
+	private Button unbindService;
+	
+	private MyService.DownloadBinder downloadBinder;
+	
+	
+	private ServiceConnection connection = new ServiceConnection() {
+		@Override
+		public void onServiceDisconnected(ComponentName name) {	
+		}
+		
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			downloadBinder = (MyService.DownloadBinder)service;
+			downloadBinder.startDownload();
+			downloadBinder.getProgress();
+		}
+	};
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -22,8 +44,14 @@ public class MainActivity extends Activity implements OnClickListener{
 		startService = (Button)findViewById(R.id.start_service);
 		stopService = (Button)findViewById(R.id.stop_service);
 		
+		bindService = (Button)findViewById(R.id.bind_service);
+		unbindService = (Button)findViewById(R.id.unbind_service);
+		
 		startService.setOnClickListener(this);
 		stopService.setOnClickListener(this);
+		
+		bindService.setOnClickListener(this);
+		unbindService.setOnClickListener(this);
 	}
 
 	@Override
@@ -36,6 +64,15 @@ public class MainActivity extends Activity implements OnClickListener{
 		case R.id.stop_service:
 			Intent stopIntent = new Intent(this, MyService.class);
 			stopService(stopIntent);
+			break;
+		case R.id.bind_service:
+			Intent bindIntent = new Intent(this, MyService.class);
+			// 绑定服务
+			bindService(bindIntent, connection, BIND_AUTO_CREATE);
+			break;
+		case R.id.unbind_service:
+			// 解绑服务
+			unbindService(connection);
 			break;
 		default:
 			break;
--- ServiceTest/src/com/example/servicetest/MyService.java
+++ ServiceTest/src/com/example/servicetest/MyService.java
@@ -1,21 +1,58 @@
 package com.example.servicetest;
 
+//import com.example.notificationtest.R;
+
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.util.Log;
+import android.os.Binder;
 
 public class MyService extends Service {
+	
+	private DownloadBinder mBinder = new DownloadBinder();
+	
+	class DownloadBinder extends Binder {
+		public void startDownload() {
+			Log.d("MyService", "startDownload executed");
+		}
+		
+		public int getProgress() {
+			Log.d("MyService", "getProgress executed");
+			return 0;
+		}
+	}
 
 	@Override
 	public IBinder onBind(Intent intent) {
 		// TODO Auto-generated method stub
-		return null;
+		Log.d("MyService", "onBind executed");
+		return mBinder;
 	}
 	
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		//Notification notification = new Notification(R.drawable.ic_launcher, 
+			//	"Notification comes", System.currentTimeMillis());
+		Intent ntfIntent = new Intent(this, MainActivity.class);
+		PendingIntent pi = PendingIntent.getActivity(this, 0, ntfIntent, 0);
+		//notification.setLatestEventInfo(this, "This is title", "This is content", pi);
+		// use Notification.Builder instead (minimum sdk version is 16)
+					Notification notice = new Notification.Builder(this)
+						.setContentTitle("This is content title")
+						.setContentText("This is content text")
+						.setSmallIcon(R.drawable.ic_launcher)
+						.setTicker("This is ticker text")
+						// setLatestEventInfo is deprecated
+						//notice.setLatestEventInfo(this, "This is content title", "This is content text", pi);
+						// use setContentIntent(PendingIntent) instead
+						.setContentIntent(pi)
+						.build();
+		startForeground(1, notice);
+		
 		Log.d("MyService", "onCreate executed");
 	}
 	
